a3f8b8fb8e33f1db404e09036bfeaec8b42742d1
AMERJOD\Amer Jod <Amer.j.eng@gmail.com>
2018-06-18 03:59:54 +0100

    Modify the Webserver: - change the port to 80 - change the webpage + add css - store request in json file

--- WebServer/app/views.py
+++ WebServer/app/views.py
@@ -1,15 +1,64 @@
+import json
+import os
+
 from flask import render_template, redirect, url_for, request
 from datetime import datetime
 from app import app, db
 from .models import Request
 
+
+JsonRequestsPATH = 'JSON/HTTPRequestNodes'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    r =  Request(ip=request.remote_addr, datetime=datetime.now())
-    db.session.add(r)
+    req =  Request(ip=request.remote_addr, datetime=datetime.now())
+    # store it in json file
+    storeHTTPRequestJSON(time=str(datetime.now()),srcIP=request.remote_addr)
+    db.session.add(req)
     db.session.commit()
-
-    # stroe in json file
-
     return render_template("index.html")
+
+
+
+#TODO: need to implmment a class for it
+def storeHTTPRequestJSON(time,srcIP):
+    """Help for the bar method of Foo classes"""
+    date = getTime(2)
+    # TODO: need refactoring - make it more abstract
+    file = JsonRequestsPATH +'_'+ date + '.json'
+    jsons = {}
+
+    if (os.path.exists(file)) != True: # check if the file exist, if not create it.
+        with open(file, 'w+') as jsonfile:
+            json.dump(' ', jsonfile)
+    else:
+        with open(file, 'r') as jsonfile:
+            jsons = json.load(jsonfile)
+
+    with open(file,'w') as jsonfile:
+        DNSRequestNodes = {
+            'Request': {
+                'ID': str(len(jsons)+1),
+                'Time': time,
+                'SrcIP': srcIP,
+            }
+        }
+        jsons[str(len(jsons)+1)] = DNSRequestNodes
+        # Write into Json file
+        json.dump(jsons, jsonfile)
+
+
+
+
+# option: 1 full (time+date)
+# option: 2 date
+# option: 3 time
+def getTime(opt = 1):
+    date = datetime.now()
+    if opt == 1:    # full
+        return (((str(date)).split('.')[0]).split(' ')[1] + ' ' + ((str(date)).split('.')[0]).split(' ')[0])
+    if opt == 2:    # date
+        return (((str(date)).split('.')[0]).split(' ')[0])
+    if opt == 3:    # time
+        return (((str(date)).split('.')[0]).split(' ')[1])
--- WebServer/run.py
+++ WebServer/run.py
@@ -1,4 +1,18 @@
 #!flask/bin/python3
+import sys
 from app import app
+
 app.debug = True
-app.run(host="0.0.0.0")
+app.static_folder = 'static'
+
+def main(argv):
+    try:
+        if argv[0] == '-s': # on the server
+            app.run(host="0.0.0.0",port="80")
+    except:
+        app.run()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
